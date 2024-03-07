dee2aa447bb394be981894a2e27f621f6255a29e
JMeckel <Jan.Meckelholt@mercedes-benz.com>
2024-03-07 14:23:12 +0100

    some styling

---
+++ app/src/main/java/com/udacity/Constants.kt
@@ -0,0 +1,8 @@
+package com.udacity
+
+object Constants {
+    const val URL_GLIDE = "https://github.com/bumptech/glide"
+    const val URL_UDACITY = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter"
+    const val URL_RETROFIT = "https://github.com/square/retrofit"
+
+}
