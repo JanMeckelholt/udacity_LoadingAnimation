712400c79f44dc98d96714a3e01d751fcdd15610
JMeckel <Jan.Meckelholt@mercedes-benz.com>
2024-03-08 08:42:18 +0100

    loading animation

--- app/src/main/java/com/udacity/loadingbtn/LoadingButton.kt
+++ app/src/main/java/com/udacity/loadingbtn/LoadingButton.kt
@@ -1,25 +1,27 @@
 package com.udacity.loadingbtn
 
-import android.animation.ObjectAnimator
-import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
+import android.os.CountDownTimer
 import android.util.AttributeSet
 import android.view.View
 import com.udacity.R
 import timber.log.Timber
 import kotlin.properties.Delegates
 
+
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
-    private var widthSize = 200
-    private var heightSize = 100
+    private var widthSize = 0
+    private var heightSize = 0
     private var buttonText = ""
+    private var progress = 0f
+    private lateinit var rectF : RectF
 
-    private val valueAnimator = ValueAnimator()
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
@@ -32,10 +34,11 @@ class LoadingButton @JvmOverloads constructor(
         textSize = resources.getDimension(R.dimen.defaultTextSize)
         textAlign = Paint.Align.CENTER
     }
-    private val rectInset = resources.getDimension(R.dimen.rectInset)
+
 
 
     init {
+        buttonState = ButtonState.Clicked
         isClickable = true
         buttonText = resources.getString(R.string.button_download)
     }
@@ -43,17 +46,40 @@ class LoadingButton @JvmOverloads constructor(
     override fun performClick(): Boolean {
         Timber.i("btn clicked")
         if (super.performClick()) return true
-        buttonText = resources.getString(R.string.button_loading)
+        if (buttonState == ButtonState.Clicked) {
+            buttonState = ButtonState.Loading
+            buttonText = resources.getString(R.string.button_loading)
+            animateProgressColor()
+        }
 
-        invalidate()
+
         return true
     }
 
     override fun onDraw(canvas: Canvas) {
         canvas.drawColor(resources.getColor(R.color.colorPrimary))
+        when (buttonState){
+            ButtonState.Clicked -> {
+
+            }
+            ButtonState.Loading -> {
+                canvas.save()
+                paint.color = resources.getColor(R.color.colorPrimaryDark)
+                paint.style = Paint.Style.FILL
+
+                canvas.drawRect(rectF, paint)
+                canvas.restore()
+            }
+            ButtonState.Completed -> {
+
+            }
+        }
+        canvas.save()
         paint.color = Color.WHITE
         canvas.translate((widthSize / 2).toFloat(), (heightSize / 2 + paint.textSize / 2))
         canvas.drawText(buttonText, 0f, 0f, paint)
+        canvas.restore()
+
         super.onDraw(canvas)
 
     }
@@ -68,16 +94,28 @@ class LoadingButton @JvmOverloads constructor(
         )
         widthSize = w
         heightSize = h
+        Timber.i("width: $widthSize, height: $heightSize")
         setMeasuredDimension(w, h)
     }
 
     private fun animateProgressColor() {
-        val animator = ObjectAnimator.ofArgb(this, "backgroundColor", Color.BLACK, Color.RED)
-        animator.setDuration(500)
-        animator.repeatCount = 1
-        animator.repeatMode = ObjectAnimator.REVERSE
-        //animator.disableViewDuringAnimation(colorizeButton)
-        animator.start()
+        Timber.i("animate loading")
+        val timeToDownload = 3_000L
+        val timer = object: CountDownTimer(timeToDownload, 100) {
+            override fun onTick(millisUntilFinished: Long) {
+                progress = ((timeToDownload - millisUntilFinished).toFloat() / timeToDownload)
+                Timber.i("progress: $progress - $timeToDownload, $millisUntilFinished")
+                rectF = RectF(0f, 0f, progress * widthSize, heightSize.toFloat())
+                invalidate()
+            }
+            override fun onFinish() {
+                progress = 1.0f
+                invalidate()
+            }
+
+        }
+        timer.start()
     }
+
 
 }
